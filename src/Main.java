a46ea620584f39fbef30d391f32072b10780b3b9
moses <moseskplai@gmail.com>
2023-11-03 08:16:33 +0800

    Added 3 methods

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
+    public static int numberOfVowels(String s){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
     
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
@@ -14,6 +23,15 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int inte){
+        int count2 = 0;
+        for (int i = 0; i < inte+1; i++){
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0){
+                count2++;
+            }
+        }
+        return count2;
+    }
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +42,24 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String str){
+        String output = "";
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == '-' || str.charAt(i) == '_'){
+                if (i != str.length() -1){
+                    String capital = str.substring(i+1,i+2);
+                    output = output + capital.toUpperCase();
+                    i++;
+                }
 
+            }
+            else {
+                output = output + str.charAt(i);
+            }
+
+        }
+        return output;
+
+    }
 
 }
